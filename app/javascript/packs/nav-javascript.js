6d29e2088cc25cde47a61aabd0508a6a4a29f6ae
Nikita Yanovskyy <nik.yanovskyy@gmail.com>
2020-04-30 22:17:55 +0300

    landing-menu-update

--- app/javascript/packs/nav-javascript.js
+++ app/javascript/packs/nav-javascript.js
@@ -5,5 +5,14 @@ document.addEventListener("turbolinks:load", function() {
          $('nav').stop().toggleClass('toggle-btn')  
          $(this).toggleClass('toggle-burger-top-line') 
      })
+
+
+     //Landing menu
+     $('.landing').parent().find('nav').css({
+        'position' : 'absolute',
+        'z-index': '10',
+        'background-color': 'transparent',
+        'width': '100%'
+     })
 })
 
